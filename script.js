8855220e39529ce48ae359e78e0f4ed28bb54696
Crjs2025 <cristophercorrea2021@gmail.com>
2025-07-30 20:44:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Cambia esta variable manualmente según la cantidad de tareas completadas:
-let tareasCompletadas = 43;
+let tareasCompletadas = 44;
 
 const etapas = [
   { nombre: "huevo",    limite: 10, sprite: "dragon_huevo.png" },
